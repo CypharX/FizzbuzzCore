b1cd3b20800444d88d14122d2d075306476a4c0b
Anthony D <CypharX@gmail.com>
2020-10-13 17:10:52 -0400

    model added

--- FizzbuzzCore/Controllers/HomeController.cs
+++ FizzbuzzCore/Controllers/HomeController.cs
@@ -31,31 +31,44 @@ namespace FizzbuzzCore.Controllers
         [HttpPost]
         public IActionResult Solve(string fizzIn, string buzzIn)
         {
-            var fizzNum = Convert.ToInt32(fizzIn);
-            var buzzNum = Convert.ToInt32(buzzIn);
-            var output = new StringBuilder();
-            for(var index = 1; index <= 100; index++)
-            {   
-                if(index % fizzNum == 0 && index % buzzNum == 0)
-                {
-                    output.AppendLine("Fizzbuzz!!");
-                }
-                else if(index % fizzNum == 0)
-                {
-                    output.AppendLine("Fizz");
-                }
-                else if(index % buzzNum == 0)
-                {
-                    output.AppendLine("buzz");
-                }
-                else
-                {
-                    output.AppendLine(index.ToString());
-                }
-            }
+            var fbModel = new FizzbuzzModel();
+            fbModel.FizzNum = Convert.ToInt32(fizzIn);
+            fbModel.BuzzNum = Convert.ToInt32(buzzIn);
+            fbModel.Output = $"{fizzIn} was given for the Fizz-number and {buzzIn} for the Buzz-number";
 
-            ViewData["Output"] = output;
-            return View();
+            return RedirectToAction("Result", fbModel);
+        }
+        //public IActionResult Solve(string fizzIn, string buzzIn)
+        //{
+        //    var fizzNum = Convert.ToInt32(fizzIn);
+        //    var buzzNum = Convert.ToInt32(buzzIn);
+        //    var output = new StringBuilder();
+        //    for(var index = 1; index <= 100; index++)
+        //    {   
+        //        if(index % fizzNum == 0 && index % buzzNum == 0)
+        //        {
+        //            output.AppendLine("Fizzbuzz!!");
+        //        }
+        //        else if(index % fizzNum == 0)
+        //        {
+        //            output.AppendLine("Fizz");
+        //        }
+        //        else if(index % buzzNum == 0)
+        //        {
+        //            output.AppendLine("buzz");
+        //        }
+        //        else
+        //        {
+        //            output.AppendLine(index.ToString());
+        //        }
+        //    }
+
+        //    ViewData["Output"] = output;
+        //    return View();
+        //}
+        public IActionResult Result(FizzbuzzModel model)
+        {
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FizzbuzzCore/Models/FizzbuzzModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzbuzzCore.Models
+{
+    public class FizzbuzzModel
+    {
+        public int FizzNum { get; set; }
+        public int BuzzNum { get; set; }
+        public string Output { get; set; }
+    }
+}
